6e5afe584492e96fde94ca460acd54e05c129b35
pgills <paul.gildea@live.com>
2014-06-02 12:34:43 -0700

    Adding the renderTemplate function back

--- examples/handlebars-winjs/runtime/js/index.js
+++ examples/handlebars-winjs/runtime/js/index.js
@@ -1,19 +1,30 @@
 var Templates = Templates || {};
 
 (function (WinJS, Handlebars, Templates) {
-    "use strict";
-    
-    // Export so renderTemplate function can be used with declarative controls
-    Templates.render = WinJS.Utilities.markSupportedForProcessing(renderTemplate);
+	"use strict";
 
-    document.addEventListener("DOMContentLoaded", function (ev){
-        WinJS.UI.processAll().done(function (){
-            console.log("WinJS:UI:processAll");
+	var source = "<li><span>Id: </span><span>{{id}}</span> <span>Text: </span><span>{{text}}</span></li>";
+	var template = Handlebars.compile(source);
 
-            // Repeater host
-            var element = document.querySelector(".pprepeater");
-            var repeater = new WinJS.UI.Repeater(element, {data: Data.items, template: renderTemplate});
+	function renderTemplate(context) {
+		var fragment = document.createElement("div");
+		fragment.innerHTML = template(context);
 
-        });
-    });
+		// Take the first child of the fragment
+		return fragment.children[0];
+	}
+
+	// Export so renderTemplate function can be used with declarative controls
+	Templates.render = WinJS.Utilities.markSupportedForProcessing(renderTemplate);
+
+	document.addEventListener("DOMContentLoaded", function (ev){
+		WinJS.UI.processAll().done(function (){
+			console.log("WinJS:UI:processAll");
+
+			// Repeater host
+			var element = document.querySelector(".pprepeater");
+			var repeater = new WinJS.UI.Repeater(element, {data: Data.items, template: renderTemplate});
+
+		});
+	});
 })(WinJS, Handlebars, Templates);
